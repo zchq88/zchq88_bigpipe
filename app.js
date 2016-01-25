b09df66b9cbcc7e32b3af44f35663d7040e71ea2
zchq88 <zchq88@aliyun.com>
2016-01-25 13:21:39 +0800

    异步处理settimeout的BUG修改

--- app.js
+++ app.js
@@ -9,23 +9,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.get('/index.html', function (req, res) {
-    res.render('index', {
-        title: "测试"
-    }, function (err, str) {
+    res.render('index', { title: "测试" }, function (err, str) {
         res.write(str)
     })
-    var Pagelets_list = [
-        {name: 'test1', is_end: false},
-        {name: 'test2', is_end: false}
-    ]
+    var Pagelets_list ={
+        pagelet1:false,
+        pagelet2:false
+    }
     var data = {is: "true"};
     function is_end(Pagelets) {
-        for (var x = 0; x < Pagelets_list.length; x++) {
-            if ( Pagelets_list[x].name===Pagelets) {
-                Pagelets_list[x].is_end=true;
-            }
-            else {
-                if(Pagelets_list[x].is_end === false)return;
+        Pagelets_list[Pagelets]=true;
+        for (x in Pagelets_list) {
+            if(!Pagelets_list[x]){
+                return;
             }
         }
         res.end();
@@ -36,9 +32,8 @@ app.get('/index.html', function (req, res) {
         res.write('<script>bigpipe.set("' + Pagelets + '",' + JSON.stringify(data) + ');</script>');
         is_end(Pagelets)
     }
-
-    setTimeout(Pagelets('test1'), 2000);
-    setTimeout(Pagelets('test2'), 5000);
+    setTimeout(function(){Pagelets("pagelet1");},1000);
+    setTimeout(function(){Pagelets("pagelet2");},3000);
 });
 
 http.createServer(app).listen(3000);
